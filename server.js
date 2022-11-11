05955414218f57ee76957c04b6a56b2d233f8db4
mahedi-hasan-75 <47491953+mahedi-hasan-75@users.noreply.github.com>
2022-11-11 16:29:39 +0600

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-let express=require('express');
-let app =express();
-app.use(express.static(__dirname+'/dist/navnar'));
-app.get('/*',(req,resp)=>{
-   resp.sendFile(__dirname+'/dist/navnar/index.html')
-});
-app.listen(process.env.PORT || 8080);
